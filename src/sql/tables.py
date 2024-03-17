fe302e47b4fa0fa31a3364a30ddebbb8002f3b29
HiebertDiogo <136849409+HiebertDiogo@users.noreply.github.com>
2024-03-17 16:51:13 -0300

    Add files via upload

--- src/sql/tables.py
+++ src/sql/tables.py
@@ -6,21 +6,25 @@ operations_table = """ CREATE TABLE IF NOT EXISTS operations (
                         operacao CHAR(1),
                         Quant INT NOT NULL,
                         P_Medio NUMERIC NOT NULL,
-                        Total NUMERIC )   """
+                        Total NUMERIC,
+                        FOREING id_cliente REFERENCES clients(id_cliente) 
+                        )   """
 
 client_table = """ CREATE TABLE IF NOT EXISTS clients (
                    id_cliente SERIAL PRIMARY KEY,
                    nome TEXT NOT NULL,
                    email TEXT NOT NULL,
                    data_nasc DATE NOT NULL,
-                   cpf TEXT NOT NULL,
-                   senha TEXT NOT NULL )"""
+                   cpf CHAR(11) NOT NULL,
+                   senha TEXT NOT NULL 
+                   )"""
 
 wallets_table = """ CREATE TABLE IF NOT EXISTS wallets (
                     id_carteira SERIAL PRIMARY KEY,
                     id_cliente INT,
-                    ticker TEXT NOT NULL,
+                    ticker TEXT,
                     Quant INT,
                     P_Medio NUMERIC,
-                    Total NUMERIC
-)"""
+                    Total NUMERIC,
+                    FOREING KEY id_cliente REFERENCES clients(id_cliente) 
+                    )"""
