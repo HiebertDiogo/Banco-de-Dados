7b812cae90255fc0833d9f5d7931536a5e5c6a7d
HiebertDiogo <136849409+HiebertDiogo@users.noreply.github.com>
2024-03-18 15:59:25 -0300

    Add files via upload

--- src/sql/tables.py
+++ src/sql/tables.py
@@ -1,15 +1,3 @@
-operations_table = """ CREATE TABLE IF NOT EXISTS operations (
-                        id_operacao SERIAL PRIMARY KEY,
-                        data DATE,
-                        id_cliente INT,
-                        ticker TEXT NOT NULL,
-                        operacao CHAR(1),
-                        Quant INT NOT NULL,
-                        P_Medio NUMERIC NOT NULL,
-                        Total NUMERIC,
-                        FOREING id_cliente REFERENCES clients(id_cliente) 
-                        )   """
-
 client_table = """ CREATE TABLE IF NOT EXISTS clients (
                    id_cliente SERIAL PRIMARY KEY,
                    nome TEXT NOT NULL,
@@ -19,6 +7,18 @@ client_table = """ CREATE TABLE IF NOT EXISTS clients (
                    senha TEXT NOT NULL 
                    )"""
 
+operations_table = """ CREATE TABLE IF NOT EXISTS operations (
+                        id_operacao SERIAL PRIMARY KEY,
+                        data DATE,
+                        id_cliente INT,
+                        ticker TEXT NOT NULL,
+                        operacao CHAR(1),
+                        Quant INT NOT NULL,
+                        P_Medio NUMERIC NOT NULL,
+                        Total NUMERIC,
+                        FOREIGN KEY (id_cliente) REFERENCES clients(id_cliente) 
+                        )"""
+
 wallets_table = """ CREATE TABLE IF NOT EXISTS wallets (
                     id_carteira SERIAL PRIMARY KEY,
                     id_cliente INT,
@@ -26,5 +26,5 @@ wallets_table = """ CREATE TABLE IF NOT EXISTS wallets (
                     Quant INT,
                     P_Medio NUMERIC,
                     Total NUMERIC,
-                    FOREING KEY id_cliente REFERENCES clients(id_cliente) 
+                    FOREIGN KEY (id_cliente) REFERENCES clients(id_cliente)
                     )"""
