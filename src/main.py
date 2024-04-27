afd5ff57067afcc862a585b67330083d1d8320c6
Emilly Eduarda <emillysilvasc@gmail.com>
2024-04-26 22:57:53 -0300

    correcoes pequenas

--- src/main.py
+++ src/main.py
@@ -7,8 +7,8 @@ def main():
     # # Chama o menu principal para iniciar a aplicação
     # interface.menu_principal()
     bd = Bd_postgres()
-    bd.create_connection()  # Certifique-se de criar a conexão antes de chamar update_wallets_table()
-    #bd.create_procedure_truncate()
+    bd.create_connection() 
+    bd.create_procedure_truncate()
     bd.truncate_table("operations")
     bd.disconnect()
 if __name__ == "__main__":
